726a82122ca7d512f8bdf863b61813dcb5278fa5
aaron-galligan <azza.galligan@gmail.com>
2025-02-18 16:40:18 +1100

    got main.py transfering any type of file, not just .txt

--- py_app/main.py
+++ py_app/main.py
@@ -1,12 +1,9 @@
 import iroh
+from iroh import Iroh, PublicKey, NodeAddr, AuthorId, Query, SortBy, SortDirection, QueryOptions, path_to_key, key_to_path, NodeOptions
 import argparse
 import asyncio
+import tempfile
 
-
-
-async def content_bytes(entry, doc):
-    result = await doc.get(entry.key())
-    return result.value
 
 async def main():
     # setup event loop, to ensure async callbacks work
@@ -19,9 +16,10 @@ async def main():
     args = parser.parse_args()
 
     # create iroh node
+    dir = tempfile.TemporaryDirectory()
     options = iroh.NodeOptions()
     options.enable_docs = True
-    node = await iroh.Iroh.memory_with_options(options)
+    node = await Iroh.persistent_with_options(dir.name, options)
     node_id = await node.net().node_id()
     print("Started Iroh node: {}".format(node_id))
 
@@ -38,17 +36,20 @@ async def main():
         ticket = await doc.share(iroh.ShareMode.READ, iroh.AddrInfoOptions.RELAY_AND_ADDRESSES)
 
         # add data to doc
-        await doc.set_bytes(author, b"hello", b"world")
-        await doc.set_bytes(author, b"foo", b"bar")
-        await doc.set_bytes(author, b"baz", b"qux")
-
+        #file_name = input("Enter file name with file extention (file must be in same folder as main): ")
+        #file_name = "C:/Users/aaron/OneDrive/Documents/Programming/Rust/SendmeInterface/py_app/flag.png"
+        file_name = "./py_app/flag.png"
+        
+        #print(file_name)
+        with open(file_name, "rb") as f:
+            bytes = bytearray(f.read())
+        await doc.set_bytes(author, file_name.encode('utf-8'), bytes)
         print("Created doc: {}".format(doc_id))
         print("Keep this running and in another terminal run:\n\npython main.py --ticket {}".format(ticket))
     else:
         # join doc
         doc_ticket = iroh.DocTicket(args.ticket)
         doc = await node.docs().join(doc_ticket)
-        print(dir(doc))
         doc_id = doc.id()
         print("Joined doc: {}".format(doc_id))
 
@@ -59,15 +60,20 @@ async def main():
         # query all keys
         query = iroh.Query.all(None)
         keys = await doc.get_many(query)
-        print(keys[0])
-        print(dir(keys[0]))
+        #print(keys[0])
+        #print(dir(keys[0]))
         print("Data:")
         for entry in keys:
             # get key, hash, and content for each entry
             key = entry.key()
+            
             hash = entry.content_hash()
             content = await node.blobs().read_to_bytes(entry.content_hash())
-            print("{}, {} (hash: {})".format(key.decode("utf8"),content.decode("utf8"), hash))  
+            #print("{}, {} (hash: {})".format(key.decode("utf8"),content.decode("utf8"), hash))
+            #copy_path = f"copy_of_{}".format(key.decode("utf8"))
+            copy_path = f"copy_of_{key.decode('utf8')}"
+            with open(copy_path, "wb") as file:
+                file.write(content)
 
 
     input("Press Enter to exit...")
--- py_app/tests/testing.py
+++ py_app/tests/testing.py
@@ -1,18 +1,9 @@
-import asyncio
 
-async def run(cmd):
-    proc = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
-
-    stdout, stderr = await proc.communicate()
-
-    print(f'[{cmd!r} exited with {proc.returncode}]')
-    if stdout:
-        print(f'[stdout]\n{stdout.decode()}')
-    if stderr:
-        print(f'[stderr]\n{stderr.decode()}')
+import os
+print("Current working directory:", os.getcwd())
 
 
-asyncio.run(run('echo hello world'))
+f = open("C:/Users/aaron/OneDrive/Documents/Programming/Rust/SendmeInterface/py_app/flag.png", "rb")
+bytes = bytearray(f.read())
+print(bytes)
+f.close()
